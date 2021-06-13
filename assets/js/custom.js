d2f35f8b18e24e3edbddcaa3b5e73fba4cf91900
Pratyush Nalam <pratyushnalam@outlook.com>
2021-06-13 17:25:31 +0530

    Persist user theme preference

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,25 @@
 // Set dark mode based on system theme
-// Provide a toggle for user to override
+// Provide a toggle for user to override and store their preference in a cookie
 
-(function() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+(function () {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') == null) {
         document.documentElement.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    }
+
+    let userPreference = localStorage.getItem('theme') || '';
+    if (userPreference) {
+        document.documentElement.classList.add(userPreference);
     }
 })();
 
 function themeToggle() {
     document.documentElement.classList.toggle('dark-mode');
+
+    let userPreference = localStorage.getItem('theme');
+    if (userPreference && userPreference === 'dark-mode') {
+        localStorage.setItem('theme', '');
+    } else {
+        localStorage.setItem('theme', 'dark-mode');
+    }
 }
