dfbba3a95cdb568f4b266baf1e60e641f4cf291b
Pratyush Nalam <pratyushnalam@outlook.com>
2021-06-13 21:35:18 +0530

    Updating default behavior of setting cookies

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,34 +1,20 @@
 // Set dark mode based on system theme
 // Provide a toggle for user to override and store their preference in a cookie
 
-// (function () {
-//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') == null) {
-//         document.documentElement.classList.add('dark-mode');
-//         localStorage.setItem('theme', 'dark-mode');
-//     }
-
-//     let userPreference = localStorage.getItem('theme') || '';
-//     if (userPreference) {
-//         document.documentElement.classList.add(userPreference);
-//     }
-// })();
-
-// function themeToggle() {
-//     document.documentElement.classList.toggle('dark-mode');
-
-//     let userPreference = localStorage.getItem('theme');
-//     if (userPreference && userPreference === 'dark-mode') {
-//         localStorage.setItem('theme', '');
-//     } else {
-//         localStorage.setItem('theme', 'dark-mode');
-//     }
-// }
-
-(function() {
+(function () {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') == null) {
         document.documentElement.setAttribute('color-mode', 'dark');
         document.documentElement.classList.add('dark-mode');
         localStorage.setItem('theme', 'dark');
+    }
+
+    var theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('color-mode', 'dark');
+        document.documentElement.classList.add('dark-mode');
+    } else {
+        document.documentElement.setAttribute('color-mode', 'light');
+        document.documentElement.classList.remove('dark-mode');
     }
 })();
 
@@ -46,6 +32,6 @@ var toggleColorMode = function toggleColorMode(e) {
 
 var toggleColorButtons = document.querySelectorAll('.color-mode-btn');
 
-toggleColorButtons.forEach(function(btn) {
+toggleColorButtons.forEach(function (btn) {
     btn.addEventListener('click', toggleColorMode);
 });
