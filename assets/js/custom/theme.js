912a8f1d194bfb12d95bfb4d6ccf1b554e441948
Pratyush Nalam <pratyushnalam@outlook.com>
2021-06-21 20:48:23 +0530

    Calling Hyvor to change palette dynamically on theme change

--- assets/js/custom/theme.js
+++ assets/js/custom/theme.js
@@ -2,41 +2,45 @@
 // Provide a toggle for user to override and store their preference in a cookie
 var palette = null;
 
+var darkPalette = {
+    accent: "#ffffff",
+    accentText: "#000000",
+    footerHeader: "#1b1818",
+    footerHeaderText: "#cac7c7",
+    box: "#232121",
+    boxText: "#ffffff",
+    boxLightText: "#aaaaaa",
+    backgroundText: "#ffffff"
+};
+
+var lightPalette = {
+    accent: "#000000",
+    accentText: "#ffffff",
+    footerHeader: "#fafafa",
+    footerHeaderText: "#484848",
+    box: "#ffffff",
+    boxText: "#111111",
+    boxLightText: "#aaaaaa",
+    backgroundText: "#111111"
+};
+
 (function () {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') == null) {
         document.documentElement.setAttribute('color-mode', 'dark');
         document.documentElement.classList.add('dark-mode');
         localStorage.setItem('theme', 'dark');
-        palette = {
-            accent: "#ffffff",
-            accentText: "#000000",
-            footerHeader: "#1b1818",
-            footerHeaderText: "#cac7c7",
-            box: "#232121",
-            boxText: "#ffffff",
-            boxLightText: "#aaaaaa",
-            backgroundText: "#ffffff"
-        };
+        palette = darkPalette;
     }
 
     var theme = localStorage.getItem('theme');
     if (theme === 'dark') {
         document.documentElement.setAttribute('color-mode', 'dark');
         document.documentElement.classList.add('dark-mode');
-        palette = {
-            accent: "#ffffff",
-            accentText: "#000000",
-            footerHeader: "#1b1818",
-            footerHeaderText: "#cac7c7",
-            box: "#232121",
-            boxText: "#ffffff",
-            boxLightText: "#aaaaaa",
-            backgroundText: "#ffffff"
-        };
+        hyvor_talk.setPalette(darkPalette);
     } else {
         document.documentElement.setAttribute('color-mode', 'light');
         document.documentElement.classList.remove('dark-mode');
-        palette = null;
+        hyvor_talk.setPalette(lightPalette);
     }
 })();
 
@@ -45,21 +49,12 @@ var toggleColorMode = function toggleColorMode(e) {
         document.documentElement.setAttribute('color-mode', 'light');
         document.documentElement.classList.remove('dark-mode');
         localStorage.setItem('theme', 'light');
-        palette = null;
+        hyvor_talk.setPalette(lightPalette);
     } else {
         document.documentElement.setAttribute('color-mode', 'dark');
         document.documentElement.classList.add('dark-mode');
         localStorage.setItem('theme', 'dark');
-        palette = {
-            accent: "#ffffff",
-            accentText: "#000000",
-            footerHeader: "#1b1818",
-            footerHeaderText: "#cac7c7",
-            box: "#232121",
-            boxText: "#ffffff",
-            boxLightText: "#aaaaaa",
-            backgroundText: "#ffffff"
-        };
+        hyvor_talk.setPalette(darkPalette);
     }
 };
 
