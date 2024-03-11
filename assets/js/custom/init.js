d56cba48dd45b0d2b636ca62c8bb882e803039fd
Pratyush Nalam <code@pratyushnalam.com>
2024-03-11 23:29:26 +0530

    Fixing tweet theme selection on page load

--- assets/js/custom/init.js
+++ assets/js/custom/init.js
@@ -5,20 +5,39 @@
         document.documentElement.setAttribute('color-mode', 'dark');
         document.documentElement.classList.add('dark-mode');
         localStorage.setItem('theme', 'dark');
-        document.querySelector('hyvor-talk-comments').setAttribute('colors', 'dark');
+        if (document.querySelector('hyvor-talk-comments')) {
+            document.querySelector('hyvor-talk-comments').setAttribute('colors', 'dark');
+        }
         setTimeout(function () {
             var tweets = document.querySelectorAll('[data-tweet-id]');
             tweets.forEach(function (tweet) {
                 var src = tweet.getAttribute('src');
                 tweet.setAttribute('src', src.replace('theme=light', 'theme=dark'));
             });
-        }, 1000);
+        }, 2000);
     }
 
     var theme = localStorage.getItem('theme');
     if (theme === 'light') {
-        document.querySelector('hyvor-talk-comments').setAttribute('colors', 'light');
+        if (document.querySelector('hyvor-talk-comments')) {
+            document.querySelector('hyvor-talk-comments').setAttribute('colors', 'light');
+        }
+        setTimeout(function () {
+            var tweets = document.querySelectorAll('[data-tweet-id]');
+            tweets.forEach(function (tweet) {
+                var src = tweet.getAttribute('src');
+                tweet.setAttribute('src', src.replace('theme=dark', 'theme=light'));
+            });
+        }, 2000);
     } else if (theme === 'dark') {
-        document.querySelector('hyvor-talk-comments').setAttribute('colors', 'dark');
+        if (document.querySelector('hyvor-talk-comments')) {
+            document.querySelector('hyvor-talk-comments').setAttribute('colors', 'dark');
+        } setTimeout(function () {
+            var tweets = document.querySelectorAll('[data-tweet-id]');
+            tweets.forEach(function (tweet) {
+                var src = tweet.getAttribute('src');
+                tweet.setAttribute('src', src.replace('theme=light', 'theme=dark'));
+            });
+        }, 2000);
     }
 })();
